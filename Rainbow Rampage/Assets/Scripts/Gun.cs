de9f1cb2ad16d12da371c7839a8c580409120e81
yoavco77 <yoavco77@gmail.com>
2023-03-21 23:07:08 +0200

    commit 4
    Fixed the bullet cooldown and range.

--- Rainbow Rampage/Assets/Scripts/Gun.cs
+++ Rainbow Rampage/Assets/Scripts/Gun.cs
@@ -8,29 +8,21 @@ public class Gun : MonoBehaviour
     public float bulletSpeed;
     public GameObject bulletPrefab;
     public float cooldown;
-    private float cooldownTimer;
     private bool canFire = true;
     public int bulletDamage;
-    private float timer;
     public float bulletRange;
 
 
     void Update()
     {
-        timer += Time.deltaTime * 3;
-        //making a cooldwon on the gun
-        cooldownTimer += Time.deltaTime * 3;
-        if (cooldownTimer > cooldown)
-        {
-            canFire = true;
-            cooldownTimer = 0;
-        }
+      
+       
 
         //looking for input and shooting
-        if (Input.GetKeyDown(KeyCode.Space) && canFire)
+        if (Input.GetKeyDown(KeyCode.Space) && canFire == true)
         {
-
             shoot();
+            StartCoroutine(cooldownWait());
 
         }
 
@@ -43,18 +35,25 @@ public class Gun : MonoBehaviour
     //shoot func
     void shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, new Vector3(spawner.position.x, spawner.position.y, 1), spawner.rotation);
         canFire = false;
+        GameObject bullet = Instantiate(bulletPrefab, new Vector3(spawner.position.x, spawner.position.y, 1), spawner.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(spawner.right * bulletSpeed, ForceMode2D.Impulse);
-        
-        if (timer > bulletRange)
-        {
-            Destroy(bullet);
-            timer = 0;
-        }
+        StartCoroutine(bulletDespawn(bullet));
 
 
     }
 
+   
+    IEnumerator cooldownWait()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canFire = true;
+    }
+
+    IEnumerator bulletDespawn(GameObject bullet)
+    {
+        yield return new WaitForSeconds(bulletRange);
+        Destroy(bullet);
+    }
 }
