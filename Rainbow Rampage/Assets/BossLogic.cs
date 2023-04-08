d09c28c782a20997569602179f32290195cb69b5
yoavco77 <yoavco77@gmail.com>
2023-04-08 13:38:23 +0300

    Commit 7
    Started the boss movement, it is not done yet.

--- Rainbow Rampage/Assets/BossLogic.cs
+++ Rainbow Rampage/Assets/BossLogic.cs
@@ -8,8 +8,19 @@ public class BossLogic : MonoBehaviour
 
     public float attackCountdown;
     public float attackRate;
+    public float stopTime;
+
 
     public float missleSpawnRadius = 3f;
+
+    //Points
+    public GameObject point1;
+    public GameObject point2;
+    public GameObject point3;
+    public GameObject point4;
+    int currentPoint = 1;
+
+    public bool isMoving = false;
 
     void Start()
     {
@@ -18,19 +29,25 @@ public class BossLogic : MonoBehaviour
 
     void Update()
     {
-        attackCountdown -= Time.deltaTime;
+        //attackCountdown -= Time.deltaTime;
 
-        if (attackCountdown <= 0)
+        /* if (attackCountdown <= 0)
+         {
+             spawnMissles();
+             attackCountdown = attackRate;
+         }*/
+
+        if (!isMoving)
         {
-            spawnMissles();
-            attackCountdown = attackRate;
+            StartCoroutine(startMoving());
         }
     }
+
     void spawnMissles()
     {
-        
+
         int randomMissleAmount = Random.Range(4, 8);
-        
+
         while (randomMissleAmount != 0)
         {
             Vector3 spawnPoint = transform.position + Random.insideUnitSphere * missleSpawnRadius;
@@ -40,5 +57,65 @@ public class BossLogic : MonoBehaviour
 
     }
 
-    
+    void movement()
+    {
+        if (currentPoint > 4)
+        {
+            currentPoint = 1;
+        }
+
+        if (currentPoint == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, point2.transform.position, 1f);
+        }
+        else if (currentPoint == 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, point3.transform.position, 1f);
+        }
+        else if (currentPoint == 3)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, point4.transform.position, 1f);
+        }
+        else if (currentPoint == 4)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, point1.transform.position, 1f);
+        }
+
+        if (Vector2.Distance(transform.position, GetTargetPosition()) < 0.1f)
+        {
+            isMoving = false;
+            currentPoint++;
+        }
+    }
+
+    IEnumerator startMoving()
+    {
+        isMoving = true;
+        movement();
+        yield return new WaitForSeconds(stopTime);
+    }
+
+    Vector2 GetTargetPosition()
+    {
+        if (currentPoint == 1)
+        {
+            return point2.transform.position;
+        }
+        else if (currentPoint == 2)
+        {
+            return point3.transform.position;
+        }
+        else if (currentPoint == 3)
+        {
+            return point4.transform.position;
+        }
+        else if (currentPoint == 4)
+        {
+            return point1.transform.position;
+        }
+        else
+        {
+            return transform.position;
+        }
+    }
 }
